9315492775327d199fa7282389bda47b5469cfdb
Ishan Joshi <ishan.a.devel@gmail.com>
2025-06-09 23:32:17 +1000

    Initial commit

---
+++ test/remote-mcp-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RemoteMcpAws from '../lib/remote-mcp-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/remote-mcp-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RemoteMcpAws.RemoteMcpAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
